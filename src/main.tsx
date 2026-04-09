af0d703f218b8659ee3229d1ef672071f97eeead
timicorpvn-stack <timicorp.vn@gmail.com>
2026-04-09 19:23:49 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-typescript
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  
-    
-  ,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
