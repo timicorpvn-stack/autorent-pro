3d26470dda03e749ec68ebea2c4fecaff7b315a6
timicorpvn-stack <timicorp.vn@gmail.com>
2026-04-09 18:58:55 +0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+```typescript
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+createRoot(document.getElementById('root')!).render(
+  
+    
+  ,
+)
+```
